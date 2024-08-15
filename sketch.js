79bacd9106f96993aa86cb788e83c6c05207b2c1
Sérgio Tafine <focoensinoonline@gmail.com>
2024-08-15 19:28:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,167 @@
+//Sprites
+var trex, chao,imagemChao, subchao,nuvem, cacto, escolherCacto, tempoJogo;
+
+//Imagens e animações
+var trexCorrendo,imagemChao, imagemNuvem;
+
+//Imagens Cactos
+var imagemCacto1, imagemCacto2, imagemCacto3, imagemCacto4, imagemCacto5, imagemCacto6;
+
+const jogar = 1;
+const encerrar = 0;
+var estadoJogo = jogar;
+
+
+function preload(){
+  
+   
+  trexCorrendo =          loadAnimation("trex1.png","trex2.png","trex3.png")
+  
+  trexColidiu = loadAnimation("trex_collided.png")
+  
+  imagemChao = loadImage("ground2.png")
+  
+  imagemNuvem = loadImage("cloud.png")
+  
+  imagemCacto1 = loadImage("cacto1.png")
+  imagemCacto2 = loadImage("cacto1.png")
+  imagemCacto3 = loadImage("cacto1.png")
+  imagemCacto4 = loadImage("cacto4.png")
+  imagemCacto5 = loadImage("cacto5.png")
+  imagemCacto6 = loadImage("cacto6.png")
+  
+  imagemFim
+  
+}
+
+function setup() {
+  createCanvas(600,200)
+  
+  //criando o sprite do trex e colocando animação
+  trex=createSprite(50,100,20,40)
+  trex.addAnimation("correndo",trexCorrendo)
+  trex.addAnimation("colidiu", trexColidiu)
+  trex.scale =0.5
+  
+  //criando o sprite do chão e colocando a imagem
+  chao = createSprite(200,180,500,10)
+  chao.addAnimation("chao",imagemChao)
+  
+  //criando o subchao e deixando ele invisível
+  subchao = createSprite(200,190,500,10)
+  subchao.visible = false
+  
+  fimDeJogo = createSprite(300,80,30,30)
+  reiniciar = createSprite(300,120,30,30)
+  
+  tempoJogo =0; 
+  
+  trex.setCollider("circle",0,0,40)
+  trex.debug = true
+  
+  grupoDeCactos = new Group();
+  grupoDeNuvens = new Group();
+  
+ 
+}
+
+
+    
+ function draw(){
+   background(180)
+   
+   text("Tempo: " + tempoJogo,500,30)
+   
+
+   
+   if(estadoJogo == jogar){
+     
+     tempoJogo = tempoJogo + Math.round(frameCount/60)
+     chao.velocityX=-2
+     
+       
+   if (chao.x < 0){
+     chao.x = chao.width/2
+   }
+   
+   if(keyDown("space")&& trex.y >161){
+      trex.velocityY = -10
+      }
+     
+   trex.velocityY = trex.velocityY + 0.5
+     
+   
+   gerarNuvens()
+   gerarCactos()
+     
+     if(grupoDeCactos.isTouching(trex)){
+       estadoJogo = encerrar
+     }
+     
+   }else if(estadoJogo == encerrar){
+      
+     chao.velocityX = 0
+     
+     grupoDeNuvens.setVelocityXEach(0);
+     grupoDeCactos.setVelocityXEach(0);
+     
+     grupoDeNuvens.setLifetimeEach(-1);
+     grupoDeCactos.setLifetimeEach(-1);
+     
+     trex.changeAnimation("colidiu", trexColidiu)
+     trex.velocityY = 0;
+     
+     
+   }
+   
+   
+   trex.collide(subchao)
+   
+   drawSprites()
+   
+ }
+
+function gerarCactos(){
+    if (frameCount % 60 == 0){
+     cacto = createSprite(600,165,10,40)
+     cacto.velocityX = -3
+      
+     escolherCacto =Math.round(random(1,6)) 
+     switch(escolherCacto){
+       case 1 : cacto.addImage(imagemCacto1)
+         break;
+       case 2 : cacto.addImage(imagemCacto2)
+         break;  
+       case 3 : cacto.addImage(imagemCacto3)
+         break;
+       case 4 : cacto.addImage(imagemCacto4)
+         break;  
+       case 5 : cacto.addImage(imagemCacto5)
+         break;     
+       case 6 : cacto.addImage(imagemCacto6)
+         break;
+       default: break;  
+     }  
+     cacto.scale = 0.4
+      cacto.lifetime = 300
+       grupoDeCactos.add(cacto);
+    }
+}
+
+function gerarNuvens(){
+  
+  if (frameCount % 60 == 0){
+     nuvem = createSprite(600,100,50,10)
+     nuvem.velocityX = -3
+    
+    nuvem.addAnimation("nuvem passando", imagemNuvem)
+    nuvem.y = Math.round(random(60,100))
+   
+    nuvem.depth = trex.depth
+    trex.depth = trex.depth + 1 
+         nuvem.scale =0.4
+    nuvem.lifetime = 300
+     grupoDeNuvens.add(nuvem);
+  }
+ 
+}
